ec4970f26bd05fc48301817cb6a227a23ae2d8d8
klaudia warmus <klaudia.warmus@student.howest.be>
2021-11-09 23:13:10 +0100

    Local API functions added

--- script/app.js
+++ script/app.js
@@ -1,3 +1,5 @@
+let htmlHolidays;
+
 const drawChart = () => {
   var options = {
     chart: {
@@ -118,17 +120,19 @@ const showResult = (queryResponse) => {
   const dayOfWeek = document.querySelector('.js-day-of-week');
   const dayOfMonth = document.querySelector('.js-day-of-month');
   const month = document.querySelector('.js-month');
+  const yearNow = document.querySelector('.js-year');
   const daysLeftThisYear = document.querySelector('.js-days-left-this-year');
 
-  // In mijn huidige versie van JS functie getYear() trekt 1900 af van het huidige jaar
-  // om het in orde te krijgen heb ik dus weer 1900 moeten bijrekenen.
-  const year = new Date(queryResponse.datetime).getYear() + 1900;
+  const year = new Date(queryResponse.datetime).getUTCFullYear();
 
   dayOfYear.innerHTML = `${queryResponse.day_of_year}`;
   dayOfWeek.innerHTML = getWeekday(queryResponse.day_of_week);
-  dayOfMonth.innerHTML = new Date(queryResponse.datetime).getDay();
-  month.innerHTML = getMonthName(new Date(queryResponse.datetime).getMonth() + 1);
+  dayOfMonth.innerHTML = new Date(queryResponse.datetime).getUTCDate();
 
+  // +1 omdat de maanden tellen van 0 (januari)
+  month.innerHTML = getMonthName(new Date(queryResponse.datetime).getUTCMonth() + 1);
+
+  yearNow.innerHTML = year;
   // Hier kijk ik of het huidig jaar een schrikkeljaar is
   // om een correcte hoeveelheid resterende dagen te berekenen
   if (leapYear(year) == true) {
@@ -138,17 +142,68 @@ const showResult = (queryResponse) => {
   }
 };
 
+const showHoliday = (queryResponse) => {
+  let InnerHTML = '';
+  for (let holiday of queryResponse) {
+    InnerHTML += `<option value="${holiday.abbr}">${holiday.name}</option>`;
+  }
+  htmlHolidays.innerHTML += InnerHTML;
+};
+
+// const calculateDays = () => {
+//   const btn = document.querySelector('.js-btn');
+
+//   // btn.onclick = (e) => {
+//   //   e.preventDefault();
+//   //   htmlHolidays.addEventListener('input', () => {
+//   //     selectedHoliday.innerHTML = htmlHolidays.value;
+//   //   });
+
+//   btn.addEventListener('click', () => {
+//     checkInput();
+//   });
+// };
+
+const addEventListeners = () => {
+  const btn = document.querySelector('.js-btn');
+  btn.addEventListener('click', () => {
+    checkInput();
+  });
+};
+const checkInput = () => {
+  console.log('checking...');
+  const selectedHoliday = document.querySelector('.js-selected-holiday');
+  htmlHolidays.addEventListener('input', () => {
+    selectedHoliday.innerHTML = `${htmlHolidays.value}`;
+  });
+};
+
 const get = (url) => fetch(url).then((r) => r.json());
 
-const getAPI = async (timezone) => {
+const getOnlineAPI = async (timezone) => {
   const endPoint = `http://worldtimeapi.org/api/timezone/${timezone}`;
   const timeResponse = await get(endPoint);
   console.log({ timeResponse });
   showResult(timeResponse);
 };
 
+const getLocalAPI = async (json) => {
+  const endPoint = `http://127.0.0.1:5500/${json}`;
+  const holidayResponse = await get(endPoint);
+  console.log({ holidayResponse });
+  showHoliday(holidayResponse);
+};
+
+const init = () => {
+  htmlHolidays = document.querySelector('.js-holiday');
+  addEventListeners();
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   console.info('DOM geladen');
+  init();
   drawChart();
-  getAPI('Europe/Brussels');
+  // calculateDays();
+  getLocalAPI('holiday.json');
+  getOnlineAPI('Europe/Brussels');
 });
